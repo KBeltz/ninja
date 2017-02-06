dd46e2966716a1d01dd8c34c39e9f3e0add20a40
Kristin Beltz <beltz08@me.com>
2017-02-05 18:49:32 -0600

    Contact form now more or less functional to send plain emails.

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    sendEmail(name, subject, message){
+      window.open('mailto:kikki.tkd@gmail.com' + '?subject=' + subject + '&body=' + message + ' Sincerely, ' + name);
+    }
+  }
 });
